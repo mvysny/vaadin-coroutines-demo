aab5ac43a60490002884a08a25fbf63fff396fbf
Martin Vysny <martin@vysny.me>
2018-10-25 21:40:55 +0300

    v bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     // heroku app runner
     staging("com.github.jsimone:webapp-runner:9.0.11.0")
 
-    testCompile("com.github.kaributesting:karibu-testing-v8:0.5.1")
+    testCompile("com.github.kaributesting:karibu-testing-v8:0.5.2")
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.11")
     testCompile("io.javalin:javalin:2.3.0")
 }
