dd84d4414deaf5fc9e41177f3be1b0822a1cd626
Martin Vysny <mavi@vaadin.com>
2024-01-19 09:32:20 +0200

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     // Vaadin
     implementation("com.vaadin:vaadin-core:${properties["vaadinVersion"]}") {
         afterEvaluate {
-            if (vaadin.productionMode) {
+            if (vaadin.productionMode.get()) {
                 exclude(module = "vaadin-dev")
             }
         }    
@@ -41,7 +41,7 @@ dependencies {
     // adds support for cancelable coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")  // https://repo1.maven.org/maven2/org/jetbrains/kotlinx/kotlinx-coroutines-core/
 
-    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 
     // simple REST support so that we can test the REST client
     implementation("io.javalin:javalin:5.6.2") {
@@ -50,7 +50,7 @@ dependencies {
         exclude(group = "com.fasterxml.jackson.core")
     }
 
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.0")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.1")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
 }
 
